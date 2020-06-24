0f5282d51df88989a536b06a45bc479eb381e38a
Sara <saraterry16@gmail.com>
2020-06-24 15:54:47 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import 'jquery';
 import 'bootstrap';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
--- webpack.config.js
+++ webpack.config.js
@@ -23,31 +23,22 @@ module.exports = {
     rules: [
       {
         test: /\.(scss|css)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              }
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            plugins: function () {
+              return [
+                require('autoprefixer')
+              ];
             }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
           }
-        ]
+        }, {
+          loader: 'sass-loader'
+        }]
       },
       {
          test: /\.(png|svg|jpg|gif)$/,
