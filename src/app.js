4a0c4394d47819a1105ddd154853164a515aba4c
Sara <saraterry16@gmail.com>
2020-06-25 12:12:02 +0100

    internal page setup

--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@ import 'bootstrap';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
 import './scss/app.scss';
+import './educational-buildings.html'
