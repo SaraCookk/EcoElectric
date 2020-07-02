d13154b0a0b7c47bfa4c3ecd4a352814988dfcb6
Sara <saraterry16@gmail.com>
2020-07-02 18:36:29 +0100

    css loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import './scripts/index.js';
+import './scripts/calculator.js';
 import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
 import './scss/app.scss';
 import './educational-buildings.html'
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
@@ -12,30 +13,40 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/template.html'
+      template: './src/template.html',
+      minify: false
     }),
     new HtmlWebpackPlugin({
       filename: 'educational-buildings.html',
-      template: './src/educational-buildings.html'
+      template: './src/educational-buildings.html',
+      minify: false
     }),
     new HtmlWebpackPlugin({
       filename: 'savings-calculator.html',
-      template: './src/savings-calculator.html'
+      template: './src/savings-calculator.html',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'content.html',
+      template: './src/content.html',
+      minify: false
     }),
     new CopyPlugin({
       patterns: [
         { from: './src/templates/header.html', to: './templates/header.html' },
         { from: './src/templates/footer.html', to: './templates/footer.html' }
       ],
-    })
+    }),
+    new MiniCssExtractPlugin({
+     filename: "[name].css"
+   })
   ],
   module: {
     rules: [
       {
         test: /\.(scss|css)$/,
-        use: [{
-          loader: 'style-loader',
-        }, {
+        use: [
+           MiniCssExtractPlugin.loader, {
           loader: 'css-loader',
         }, {
           loader: 'postcss-loader',
@@ -52,11 +63,13 @@ module.exports = {
       },
       {
          test: /\.(png|svg|jpg|gif)$/,
-         use: [{
+         use: [
+           {
            loader: 'file-loader',
            options: {
              name: '[name].[ext]',
-             outputPath: 'images/'
+             outputPath: 'images/',
+             publicPath: './images/'
            }
          }],
       },
@@ -84,5 +97,5 @@ module.exports = {
         loader: "babel-loader"
       }
     ]
-  }
+  },
 };
